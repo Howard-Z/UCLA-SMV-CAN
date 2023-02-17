69aab8adf4835a481af87ad35c219442e2cd9bb7
Howard-Z <howardzhu8@gmail.com>
2023-02-17 04:05:34 -0800

    Update

--- SMVcanbus.cpp
+++ SMVcanbus.cpp
@@ -3,52 +3,86 @@
 #include "SMVcanbus.h"
 #include "ids.h"
 
-CAN_message_t globalMessage;
 
-void callBack(const CAN_message_t &msg) //setting the global message var
+namespace SMVcanbus //we use a name space to keep track of the message that the global callback function produces.
 {
-    globalMessage = msg;
+    CAN_message_t msg; //the global message variable
+    bool isMessage = false; //is there a new message available
+
+    void callback(const CAN_message_t& mesg)
+    {
+        msg = mesg; //setting the message
+        isMessage = true; //setting the flag that there is a message available
+        //Serial.println("calling back"); //debugging
+    }
 }
+
+void testCallback(const CAN_message_t& mesg) //debugging test callback function
+{
+    Serial.println("there is an event");
+}
+
 
 CANBUS::CANBUS() //initialize the starting settings for CANBUS
 {
-    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
+    //FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0; //not actually needed bc it's taken care of in the constructor
     Can0.begin();
     Can0.setBaudRate(1000000);
     Can0.setMaxMB(16);
     Can0.enableFIFO();
     Can0.enableFIFOInterrupt();
+    Can0.onReceive(SMVcanbus::callback);
+    //Can0.onReceive(testCallback); //debugging
     Can0.mailboxStatus();
     return;
 }
 
+void CANBUS::getEvents() //see's if there's a new message, if there is, run the call back function
+{
+    Can0.events();
+}
+
+void CANBUS::parser(const CAN_message_t& mesg) //takes the message if there is a new one and processes it
+{
+    if (SMVcanbus::isMessage)
+    {
+        this->msg = SMVcanbus::msg; //sets the current message we are working with
+        SMVcanbus::isMessage = false; //changes the boolean to say that there is no new message anymore
+        setIDs(); //sets the IDs
+    }
+    return;
+}
+/* //old parse function
 void CANBUS::parse() //gets the mail and sets the global var to the message
 {
     Can0.events();
-    Can0.onReceive(callBack);
     this->setMsg();
     this->setIDs();
     this->readHardware();
     this->readDataType();
-}
+}*/
 
-void CANBUS::setMsg() //sets the message
-{
-    this->msg = globalMessage;
-}
 
-void CANBUS::send(uint8_t message, uint16_t id) //send message
+void CANBUS::send(uint8_t* message, uint16_t id) //send message (potential problem if you try and send faster than 1 per 200 ms
 {
-    CAN_message_t msg;
-    msg.id = id;
-    msg.buf[0] = message;
-    Can0.write(msg);
+    CAN_message_t mesg;
+    mesg.id = id; //set the id field
+    for (int i = 0; i < 8; i++) //buffer is broken down into 8 uint8_t sections, ,loop through them and set it
+    {
+        mesg.buf[i] = message[i];
+    }
+    Can0.write(mesg);//send the message
 }
 
 void CANBUS::setIDs()
 {
     first = msg.id << 21 >> 28; //greab the first 4 bits
     last = msg.id << 27 >> 27; //grab the last 4 bits
+}
+
+bool CANBUS::isThere()
+{
+    return SMVcanbus::isMessage; //checks if there's a message available
 }
 
 void CANBUS::readHardware() {
@@ -59,15 +93,15 @@ void CANBUS::readDataType()
 {
     switch (first)
     {
-        case 0:
-        case 1:
-            dataType = motorMessage[last];
-            break;
-        case 2:
-            dataType = jouleMessage[last];
-            break;
-        case 3:
-            dataType = steeringMessage[last];
-            break;
+    case 0:
+    case 1:
+        dataType = motorMessage[last];
+        break;
+    case 2:
+        dataType = jouleMessage[last];
+        break;
+    case 3:
+        dataType = steeringMessage[last];
+        break;
     }
 }
--- SMVcanbus.h
+++ SMVcanbus.h
@@ -8,25 +8,27 @@
 //Preface: this was designed for the can2.0 protocol, this will not work for anything else
 class CANBUS{
     public:
-    CANBUS();
-    void readFirst();
-    void readLast();
-    void parse();
-    void setMsg();
-    void send(uint8_t message, uint16_t id);
-    void setIDs();
-    String getHardware(){ return hardware; }
-    String getDataType(){ return dataType; }
-    CAN_message_t getMessage() { return msg; }
-    void readHardware();
-    void readDataType();
+        CANBUS();
+        void getEvents(); //This function must be present in the loop function to receive CAN messages
+        //void parse();
+        void parser(const CAN_message_t& mesg); //if there is a message, process it
+        void send(uint8_t* message, uint16_t id); //sends a message (takes in an 8 element array of uint8_t)
+        void setIDs(); //set the IDs based on the message
+        uint32_t getFirst() { return first; } //get the first 4 bits
+        uint32_t getLast() { return last; } //get the last 4 bits
+        String getHardware(){ return hardware; } //returns what hardware
+        String getDataType(){ return dataType; } //returns what datatype
+        CAN_message_t getMessage() { return msg; } //returns the raw CAN message
+        bool isThere(); //boolen to check if there is a new message
+        void readHardware(); //use the first 4 bits to determine hardware
+        void readDataType();//use the whole ID to determine data type
     private:
-    CAN_message_t msg;
-    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
-    uint32_t first;
-    uint32_t last;
-    String hardware;
-    String dataType;
+        FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0; //initialize the CAN library
+        CAN_message_t msg; //current message we are working with
+        uint32_t first; //first 4 bits of the ID
+        uint32_t last; //last 4 bits of the ID
+        String hardware;
+        String dataType;
 };
 
 
