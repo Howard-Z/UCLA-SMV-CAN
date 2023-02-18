6fb2da45b107a90b0d21d04a610542cf1da574a3
Howard-Z <67174263+Howard-Z@users.noreply.github.com>
2023-02-18 11:29:55 -0800

    fixed sender and receiver ino

--- test/receiver/receiver.ino
+++ test/receiver/receiver.ino
@@ -1,46 +1,22 @@
-//#include <FlexCAN_T4.h>
 #include "SMVcanbus.h"
-//FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
+
 CANBUS can;
+
 void setup(void) {
   Serial.begin(115200); delay(400);
-
-  // pinMode(6, OUTPUT); digitalWrite(6, LOW); /* optional tranceiver enable pin */
-  // Can0.begin();
-  // Can0.setBaudRate(1000000);
-  // Can0.setMaxMB(16);
-  // Can0.enableFIFO();
-  // Can0.enableFIFOInterrupt();
-  // Can0.onReceive(canSniff);
-  // Can0.mailboxStatus();
 }
 
-// void canSniff(const CAN_message_t &msg) {
-//   Serial.print("MB "); Serial.print(msg.mb);
-//   Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
-//   Serial.print("  LEN: "); Serial.print(msg.len);
-//   Serial.print(" EXT: "); Serial.print(msg.flags.extended);
-//   Serial.print(" TS: "); Serial.print(msg.timestamp);
-//   Serial.print(" ID: "); Serial.print(msg.id, HEX);
-//   Serial.print(" Buffer: ");
-//   for ( uint8_t i = 0; i < msg.len; i++ ) {
-//     Serial.print(msg.buf[i], HEX); Serial.print(" ");
-//   } Serial.println();
-// }
-
 void loop() {
-  //Can0.events();
   can.getEvents();
-  static uint32_t timeout = millis();
-  if ( millis() - timeout > 200 && can.isThere()) {
-    can.parser();
-    CAN_message_t mesg = can.getMessage();
-    for ( uint8_t i = 0; i < 8; i++ )
-    {
-      Serial.print(mesg.buf[i], DEC);
-      Serial.println();
-    }
-    timeout = millis();
+  can.parser();
+  CAN_message_t mesg = can.getMessage();
+  Serial.print("The Buffer is: ");
+  for ( uint8_t i = 0; i < 8; i++ )
+  {
+    
+    Serial.print(mesg.buf[i], DEC);
   }
-
+  Serial.print(" The ID is: ");
+  Serial.print(mesg.id);
+  Serial.println();
 }
